40363c8ae8d9663e9d4f25118f29967f1b0bcea3
dinakinyua123 <dayokishs@gmail.com>
2023-05-19 15:23:37 -0700

    task5.c

---
+++ task5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_environment - Prints the current environment
+ */
+void print_environment(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_environment();
+
+	return (0);
+}
+
+/**
+ * print_environment - Prints the current environment
+ */
+void print_environment(void)
+{
+	extern char **environ;
+	int i = 0;
+
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
+
