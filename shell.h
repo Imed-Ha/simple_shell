00af9e9723b943263e32917f60beb496b1a93a01
dinakinyua123 <dayokishs@gmail.com>
2023-05-23 21:34:21 -0700

    updated

--- shell.h
+++ shell.h
@@ -1,10 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
+#define MAX_COMMAND_LENGTH 100
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 int main(void)
 #endif
