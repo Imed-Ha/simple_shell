f6da152ee641d68a1d4caafb6504edf95987b714
dinakinyua123 <dayokishs@gmail.com>
2023-05-23 21:53:25 -0700

    updated

--- shell.h
+++ shell.h
@@ -1,6 +1,9 @@
 #ifndef SHELL_H
 #define SHELL_H
 #define MAX_COMMAND_LENGTH 100
+#define BUFFER_SIZE 1024
+#define TOKEN_DELIMITERS " \t\r\n\a"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,4 +14,13 @@
 #include <fcntl.h>
 #include <errno.h>
 int main(void)
+void execute_command(char **args);
+void handle_error(const char *message);
+void execute_command(char **args);
+void handle_error(const char *message);
+char *buffer;
+char *token;
+char **args;
+ssize_t read_bytes;
+size_t buffer_size = BUFFER_SIZE;
 #endif
