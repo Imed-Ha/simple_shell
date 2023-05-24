750c305f1b0a951399fdee1c4fc9ce5b249834ed
dinakinyua123 <dayokishs@gmail.com>
2023-05-23 21:08:11 -0700

    updated

---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(void)
+#endif
